3708fccaaa8c081fbf5acbc58015a8307c9f7662
376305 <shishuaiyun@deppon.com>
2020-11-02 15:31:35 +0800

    111

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'localhost'),// strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
-        username: env('DATABASE_USERNAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'blog'), // 新建用户叫 blog
         password: env('DATABASE_PASSWORD', 'dclwoaitkw'),
       },
       options: {
